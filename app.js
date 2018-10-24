6eceb4f02f63b8f75e0130cdce9e5f9d0b869dae
Paula Suarez <psuarez@dickeys2.local>
2018-10-24 13:10:39 -0500

    Adding updates to .js file

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-function favMovie (movie) {
-    console.log(`My favorite movie is: ${movie}.`);
+//Template Literals and Default Parameters Value
+
+function favMovie(name = 'World', movie = 'The Room') {
+
+    //template literal
+    console.log(`My name is ${name} and my favorite movie is: ${movie}.`);
 
 }
 
-favMovie('Halloween');
- 
+//passing in parameters so default parameters in the function are not used
+favMovie('Paula', 'Halloween');
+
+
+//passing no parameters so default parameters are used.
+//favMovie();
+
+//Arrow Functions
+
+let ES6 = (name = 'World', movie = 'The Room') => {
+
+    //template literal
+    console.log(`My name is ${name} and my favorite movie is: ${movie}.`);
+
+}
+
+ES6('Jane', 'Lego Movie');
+
