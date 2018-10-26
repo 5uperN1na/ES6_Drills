ded222d548d8a4d860002598a2940a9d05899ed5
Paula Suarez <psuarez@dickeys2.local>
2018-10-26 05:45:20 -0500

    Adding updates to app.js file

--- app.js
+++ app.js
@@ -25,3 +25,31 @@ let ES6 = (name = 'World', movie = 'The Room') => {
 
 ES6('Jane', 'Lego Movie');
 
+//Arrow Function with Concise body
+
+let ES62 = (name = 'Joe', movie = 'The Room') => console.log(`My name is ${name} and my favorite movie is: ${movie}.`);
+ES62('Jim', 'Taken');
+
+
+let getFirstName = (n = 'Paula Suarez') => {
+    let fName = n.split(' ');
+    return fName[0];
+    console.log(fName[0]);
+}
+
+getFirstName();
+
+
+//Concise body example
+// shorthand 
+//let add = (a, b) => a + b;
+
+// equivalent to: 
+let add = (a, b) => {
+    return a + b;
+};
+let sum = add(5, 4);
+console.log(sum);
+
+
+
